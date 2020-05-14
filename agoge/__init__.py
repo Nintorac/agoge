a12752b3ea145d49ea0dfb03ee90c31c9fa3b8cd
Nintorac <nintoracaudio@gmail.com>
2020-05-14 11:52:27 +1000

    added gcs fetch if model not found locally

--- agoge/__init__.py
+++ agoge/__init__.py
@@ -2,7 +2,8 @@ from .utils import defaults_f
 DEFAULTS = defaults_f({
     'ARTIFACTS_ROOT': '~/agoge/artifacts',
     'TQDM_ENABLED': False,
-    'TRIAL_ROOT': 'Worker'
+    'TRIAL_ROOT': 'Worker',
+    'BUCKET': 'nintorac_model_serving'
 })
 
 from .tracker import Tracker
--- agoge/inference_worker.py
+++ agoge/inference_worker.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from contextlib import suppress
 from tqdm import tqdm
 import mlflow
 import torch
@@ -6,18 +7,34 @@ from ray.tune import Trainable
 from agoge import AbstractModel as Model, Tracker, AbstractSolver as Solver, DataHandler
 from agoge.utils import to_device
 from agoge import DEFAULTS
-from agoge.utils import get_logger
+from agoge.utils import get_logger, download_blob
 
 logger = get_logger(__name__)
+
+ARTIFACTS_ROOT = DEFAULTS['ARTIFACTS_ROOT']
+BUCKET = DEFAULTS['BUCKET']
 
 
 class InferenceWorker():
 
-    def __init__(self, path, with_data=False):
+    def __init__(self, name, project, path=ARTIFACTS_ROOT, with_data=False):
 
-        self.path = Path(path).expanduser().as_posix()
-        self.with_data = with_data
-        self.setup_components() 
+            if not isinstance(path, Path):
+                path = Path(path).expanduser()
+            model_path = Path(project).joinpath(name).with_suffix('.box')
+            full_path = path.joinpath(model_path)
+            
+            
+            if not full_path.exists():
+                print('downloading')
+                with suppress(FileExistsError):
+                    full_path.parent.mkdir()
+                
+                download_blob(BUCKET, model_path.as_posix(), full_path.as_posix())
+
+            self.path = full_path.as_posix()
+            self.with_data = with_data
+            self.setup_components()
 
     def setup_components(self, **config):
 
